a3e1640665825b1087c76dcfd421cf215c5e9677
Roberto Pozzi <r.robipozzi@gmail.com>
2021-05-31 17:05:23 +0200

    implement security with Auth0

--- app/app.js
+++ app/app.js
@@ -16,6 +16,29 @@ app.use(errorHandler);
 //app.use(haltOnTimedout);
 // Variables section
 var PORT = process.env.EXPOSED_PORT || 8082;
+// #######################################
+// ###### Auth0 Authorization setup ######
+// #######################################
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+// Authorization middleware. 
+// When used, the Access Token must exist and
+// be verified against the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://robipozzi.eu.auth0.com/.well-known/jwks.json`
+  }),
+  // Validate the audience and the issuer.
+  audience: 'windfire-restaurants',
+  issuer: `https://robipozzi.eu.auth0.com/`,
+  algorithms: ['RS256']
+});
 // Routers setup
 require("./routers/health")(app, logger);
 require("./routers/restaurant")(app, logger);
